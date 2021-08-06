c2c3332dc40bd9481a61a039cd31abb68bb400b8
shashwat shukla <72383260+shashwatshukla123@users.noreply.github.com>
2021-08-06 07:17:33 +0530

    Update scatter.py

--- pro-103/scatter.py
+++ pro-103/scatter.py
@@ -2,5 +2,5 @@ import pandas as pd
 import plotly.express as px
 
 df=pd.read_csv("C:/pro-python/pro-103/data.csv")
-fig=px.scatter(df,x="date",y="cases",color="Country")
+fig=px.scatter(df,x="date",y="cases",color="country")
 fig.show()
